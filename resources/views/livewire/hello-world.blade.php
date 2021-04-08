b97dc1b149840e5d20237c731961520bf2a8c2e8
Mumuni Mohammed <mumunim10@gmail.com>
2021-04-08 07:04:56 +0000

    Add debounce on the model to control the time show data. G

--- resources/views/livewire/hello-world.blade.php
+++ resources/views/livewire/hello-world.blade.php
@@ -1,7 +1,7 @@
 <div>
     <h3>
-        <input type="text" wire:model="name">
-        
+        <input type="text" wire:model.debounce.1000ms="name">
+
         Hello {{ $name }}!
     </h3>
 </div>
